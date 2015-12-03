d652d12f08aa9bd78d6b0d87cff4ca3d85592051
Tom Needham <06needhamt@gmail.com>
2015-12-03 10:01:00 +0000

    add boilerplate project

---
+++ Test Boilerplate/main.cpp
@@ -0,0 +1,12 @@
+#include "stm32f2xx.h"
+
+#ifndef SystemInit
+int SystemInit(int a){
+	return 0;
+}
+#endif
+
+int main(void){
+	SystemInit();
+	return 0;
+}
---
+++ Test Boilerplate/system_stm32f2xx.d
@@ -0,0 +1,7 @@
+.\system_stm32f2xx.o: system_stm32f2xx.c
+.\system_stm32f2xx.o: C:\Keil\ARM\Inc\ST\STM32F2xx\stm32f2xx.h
+.\system_stm32f2xx.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\system_stm32f2xx.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\system_stm32f2xx.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\system_stm32f2xx.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\system_stm32f2xx.o: C:\Keil\ARM\Inc\ST\STM32F2xx\system_stm32f2xx.h
