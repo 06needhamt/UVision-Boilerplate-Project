f8a88fab4ee5db94d0f4ac3e96cc6afb0dc3ea77
cnicholas63 <cnicholas63@gmail.com>
2015-12-09 11:30:50 +0000

    Boiler plate based on Blinky

---
+++ Boiler Plate/Flash/boilerplate.d
@@ -0,0 +1,12 @@
+.\flash\boilerplate.o: BoilerPlate.c
+.\flash\boilerplate.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\flash\boilerplate.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\flash\boilerplate.o: C:\Keil\ARM\Inc\ST\STM32F10x\stm32f10x.h
+.\flash\boilerplate.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+.\flash\boilerplate.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\flash\boilerplate.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\flash\boilerplate.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\flash\boilerplate.o: C:\Keil\ARM\Inc\ST\STM32F10x\system_stm32f10x.h
+.\flash\boilerplate.o: GLCD.h
+.\flash\boilerplate.o: LED_Functions.h
+.\flash\boilerplate.o: LCD_Functions.h
