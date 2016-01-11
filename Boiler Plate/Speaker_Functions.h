0417990336844a2cb02e16af6f1fbac092bf7562
Tom Needham <06needhamt@gmail.com>
2016-01-11 19:26:24 +0000

    Use Gitignore

--- Boiler Plate/Speaker_Functions.h
+++
@@ -1,9 +0,0 @@
-#include "stm32f10x.h"
-
-extern void delay(int secs);
-
-void pin_PA4_For_Speaker(void);
-
-void generate_Tone_On_Speaker(int f, int d);
-
-void generate_FSK_Tone(unsigned char *s);
