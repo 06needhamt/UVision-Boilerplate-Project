18288b3461b7de1a8f74b42525b1eb18d11b089b
Tom Needham <06needhamt@gmail.com>
2016-01-11 19:24:15 +0000

    Use Gitignore

--- Boiler Plate/Flash/potentiomter_functions.d
+++
@@ -1,7 +0,0 @@
-.\flash\potentiomter_functions.o: Potentiomter_Functions.c
-.\flash\potentiomter_functions.o: G:\Keil\IDE\ARM\Inc\ST\STM32F10x\stm32f10x.h
-.\flash\potentiomter_functions.o: G:\Keil\IDE\ARM\CMSIS\Include\core_cm3.h
-.\flash\potentiomter_functions.o: G:\Keil\IDE\ARM\ARMCC\bin\..\include\stdint.h
-.\flash\potentiomter_functions.o: G:\Keil\IDE\ARM\CMSIS\Include\core_cmInstr.h
-.\flash\potentiomter_functions.o: G:\Keil\IDE\ARM\CMSIS\Include\core_cmFunc.h
-.\flash\potentiomter_functions.o: G:\Keil\IDE\ARM\Inc\ST\STM32F10x\system_stm32f10x.h
--- Boiler Plate/Flash/retarget.d
+++
@@ -1,3 +0,0 @@
-.\Flash\retarget.o: Retarget.c
-.\Flash\retarget.o: C:\Keil\ARM\RV31\Inc\stdio.h
-.\Flash\retarget.o: C:\Keil\ARM\RV31\Inc\rt_misc.h
